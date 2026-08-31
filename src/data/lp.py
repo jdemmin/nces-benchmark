bd057df8a3bbd255b2b09736c2f3cca88f204b4a
Joel Demmin <joel.demmin@mail.de>
2026-08-31 16:33:14 +0200

    Patch: LPGen was not seed-dependant anymore

--- src/data/lp.py
+++ src/data/lp.py
@@ -126,7 +126,7 @@ def generate_learning_problems(
         kwargs["max_num_lps"],
         kb_path.name,
     )
-    LPGen(**kwargs).generate()
+    LPGen(**kwargs, random_seed=seed).generate()
 
     raw_path = storage_path / "LPs.json"
     if not raw_path.is_file():
